eca23842106cf587efbc639c7c0813db37ae80f1
DonovanDwyer <dwyer.donovan@gmail.com>
2018-10-08 15:50:43 -0400

    created three paths for view

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
+require 'date'
+
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+attr_accessor :name, :date
+
+	# get '/' do
+	# 	erb :index
+	# end
+
+	get '/hello' do
+		erb :hello
 	end
 
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		d = DateTime.now
+		@date = d.strftime("The date is %A, %B %d, %Y")
+		erb :date
+	end
 
 end
